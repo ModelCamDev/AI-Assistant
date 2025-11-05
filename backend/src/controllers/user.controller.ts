3ec9ca9b02bf0839c85764749eae208ab656c201
Aditya Bheke <adityabheke34@gmail.com>
2025-11-05 17:55:40 +0530

    backend: jwt payload

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -3,7 +3,7 @@ import userService from "../services/user.service";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-
+const JWT_SECRET = process.env.JWT_SECRET || 'secret-key';
 export const registerUser = async (req: Request, res: Response, next: NextFunction)=>{
     try {
         const {name, email, password, role} = req.body;
@@ -22,7 +22,7 @@ export const loginUser = async (req: Request, res: Response, next: NextFunction)
         if(!user) throw new Error("User not found");
         const isValid = await bcrypt.compare(password, user.password);
         if (!isValid) throw new Error("Invalid Credentials");
-        const token = jwt.sign({userId: user._id}, 'secret-key', {expiresIn: '1d'});
+        const token = jwt.sign({id: user._id, email: user.email, role: user.role}, JWT_SECRET, {expiresIn: '1d'});
         res.status(200).json({message:"Login success", user, token})
     } catch (error) {
         next(error)
