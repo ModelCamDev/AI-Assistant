b7712f7bff6e611a291d47897db90bee74b42936
Aditya Bheke <adityabheke34@gmail.com>
2025-11-06 14:43:55 +0530

    frontend: add react icons

--- frontend/src/components/Admin/Header.tsx
+++ frontend/src/components/Admin/Header.tsx
@@ -1,11 +1,12 @@
 import logo from './../../assets/logo1.png';
+import { FaEnvelope, FaPhone } from 'react-icons/fa6'
 
 function Header() {
 
   return (
     <>
         <div className="logo"><img src={logo} alt="modelcam" /></div>
-        <div className="contact">+91 8237016167 sales@modelcamtechnologies.com</div>
+        <div className="contact"><FaPhone size={15} style={{ verticalAlign: "middle", marginRight: '5px' }}/>&nbsp;+91 8237016167 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <FaEnvelope  size={15} style={{ verticalAlign: "middle", marginRight: '5px' }}/>&nbsp;sales@modelcamtechnologies.com</div>
     </>
   )
 }
--- frontend/src/components/Admin/Sideber.tsx
+++ frontend/src/components/Admin/Sideber.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom"
 import { useAppDispatch, useAppSelector } from "../../redux/app/hooks"
 import { logoutAdmin } from "../../redux/slices/adminSlice";
+import { FaArrowRightFromBracket, FaArrowRightToBracket, FaFileArrowUp, FaGrip, FaUserGroup } from 'react-icons/fa6'
 
 function Sidebar() {
   const {isLoggedIn} = useAppSelector((state)=>state.admin);
@@ -12,11 +13,11 @@ function Sidebar() {
   return (
     <>
     <div className="sidebar-items-list">
-    <NavLink to={'/admin'} end className={({isActive})=> isActive ? 'selected': 'unselected'}>Overview</NavLink>
-    <NavLink to={'leads'} className={({isActive})=> isActive ? 'selected': 'unselected'}>Leads</NavLink>
-    <NavLink to={'employees'} className={({isActive})=> isActive ? 'selected': 'unselected'}>Employees</NavLink>
+    <NavLink to={'/admin'} end className={({isActive})=> isActive ? 'selected': 'unselected'}><FaGrip size={20} style={{ verticalAlign: "middle", marginRight: '5px' }}/>Overview</NavLink>
+    <NavLink to={'leads'} className={({isActive})=> isActive ? 'selected': 'unselected'}><FaUserGroup size={20} style={{ verticalAlign: "middle", marginRight: '5px' }}/>Leads</NavLink>
+    <NavLink to={'employees'} className={({isActive})=> isActive ? 'selected': 'unselected'}> <FaFileArrowUp size={20} style={{ verticalAlign: "middle", marginRight: '5px' }}/> Documents</NavLink>
     </div>
-    <button onClick={handleLogout} className="logout">{isLoggedIn?"Logout":"Login"}</button>
+    <button onClick={handleLogout} className="logout">{isLoggedIn?<><FaArrowRightFromBracket size={20} style={{ verticalAlign: "middle", marginRight: '5px' }} />Logout</>:<><FaArrowRightToBracket size={20} style={{ verticalAlign: "middle", marginRight: '5px' }}/>Login</>}</button>
     </>
   )
 }
