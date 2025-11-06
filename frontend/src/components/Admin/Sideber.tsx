8671acf73b7da2ce474c267a4bb685bf4f157dc9
Aditya Bheke <adityabheke34@gmail.com>
2025-11-06 10:54:48 +0530

    frontend: implement logout feature

--- frontend/src/components/Admin/Sideber.tsx
+++ frontend/src/components/Admin/Sideber.tsx
@@ -1,12 +1,22 @@
 import { NavLink } from "react-router-dom"
+import { useAppDispatch, useAppSelector } from "../../redux/app/hooks"
+import { logoutAdmin } from "../../redux/slices/adminSlice";
 
 function Sidebar() {
+  const {isLoggedIn} = useAppSelector((state)=>state.admin);
+  const dispatch = useAppDispatch();
+  const handleLogout = ()=>{
+    dispatch(logoutAdmin());
+  }
 
   return (
     <>
+    <div className="sidebar-items-list">
     <NavLink to={'/admin'} end className={({isActive})=> isActive ? 'selected': 'unselected'}>Overview</NavLink>
     <NavLink to={'leads'} className={({isActive})=> isActive ? 'selected': 'unselected'}>Leads</NavLink>
     <NavLink to={'employees'} className={({isActive})=> isActive ? 'selected': 'unselected'}>Employees</NavLink>
+    </div>
+    <button onClick={handleLogout} className="logout">{isLoggedIn?"Logout":"Login"}</button>
     </>
   )
 }
