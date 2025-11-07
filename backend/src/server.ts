5c5125bed5095de626f983ba6fa59069c32f3360
Aditya Bheke <adityabheke34@gmail.com>
2025-11-07 17:48:17 +0530

    backend: fix cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 // Implement cors
-app.use(cors());
+app.use(cors({origin:'http://localhost:5173',credentials:true}));
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
